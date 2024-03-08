fb52f7659cfb95ab9ac24b122c610936bfd10a7f
maryamHabibpourr <Einesheikh_12@yahoo.com>
2024-03-08 15:08:41 +0330

    implement air pollution component

--- app/context/globalContext.js
+++ app/context/globalContext.js
@@ -12,13 +12,17 @@ const GlobalContextUpdate = createContext();
 
 export const GlobalContextProvider = ({children})=>{
     const [forecast, setForecast] = useState({})
+    const [airQuality, setAirQuality] = useState({});
+
+
+
 
 
 
     const fetchForecast = async ()=>{
         try{
             const res = await axios.get("api/weather")
-            console.log("res:" , res.data)
+            console.log("res-weather:" , res.data)
             setForecast(res.data);
         } catch(error){
             console.log(error.message)
@@ -26,14 +30,35 @@ export const GlobalContextProvider = ({children})=>{
     }
 
 
+
+
+
+  // Air Quality
+  const fetchAirQuality = async () => {
+    try {
+      const res = await axios.get("api/pollution");
+      console.log("resAirPollution", res.data)
+      setAirQuality(res.data);
+    } catch (error) {
+      console.log("Error fetching air quality data: ", error.message);
+    }
+  };
+
+
+
+
     useEffect(()=>{
         fetchForecast();
+        fetchAirQuality();
     },[])
 
 
     
     return(
-        <GlobalContext.Provider value={{forecast}}>
+        <GlobalContext.Provider value={{
+            forecast,
+            airQuality
+            }}>
             <GlobalContextUpdate.Provider>
                 {children}
             </GlobalContextUpdate.Provider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Temperature from "./components/Temperature/Temperature";
+import AirPollution from "./components/AirPollution/AirPollution";
 
 
 export default function Home() {
@@ -12,6 +13,9 @@ export default function Home() {
         <Temperature />
         </div>
         <div className="flex flex-col">
+        <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
+          <AirPollution/>
+        </div>
           
         </div>
       </div>
